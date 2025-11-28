d79e792412f248fe1fcc1732b8241b7edce38876
yolin9423 <yolin7723@gmail.com>
2025-11-28 19:16:37 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  publicDir: 'public', // Specify explicitly
+  base: './', // Ensures relative paths for assets
+  publicDir: 'public', // Files in here will be copied to root on build
   build: {
     outDir: 'dist',
-    target: 'es2015', // Critical for iOS compatibility
+    target: 'es2015', // Older target prevents white screen on older iOS
     cssTarget: 'chrome61',
   },
 });
